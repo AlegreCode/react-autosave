98b167651b68978efe0f973b32a2b847e993c507
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2022-12-14 18:42:56 -0300

    Agregamos el componente Form.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
+import Form from "./Form/Form";
 
 function App() {
   return (
     <div className="min-h-screen max-w-full bg-slate-900">
-      <header className="App-header">
-        <h1 className='text-4xl text-gray-600'>Hello World!</h1>
-      </header>
+      <Form />
     </div>
   );
 }
