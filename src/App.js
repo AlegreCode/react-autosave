42f8654147437be213daac4696372a002ea8cceb
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2022-12-15 18:52:26 -0300

    Establecemos un flex element para centrar el formulario

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Form from "./Form/Form";
 
 function App() {
   return (
-    <div className="min-h-screen max-w-full bg-slate-900">
+    <div className="min-h-screen max-w-full bg-slate-900 flex justify-center items-center">
       <Form />
     </div>
   );
