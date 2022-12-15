9fc4aa6c7b28e7fb90d4ac8abaeccfc06545d7b5
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2022-12-15 18:52:39 -0300

    Estilizamos el formulario

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,18 +1,17 @@
+
 function Form() {
     return (
         <>
-            <section>
+            <section className="text-white w-[600px] p-4">
+                <header className="text-white text-4xl text-center py-4">Form Autosave</header>
                 <form>
                     <div>
-                        <label>Title:</label>
-                        <input type="text" name="title" placeholder="Title" />
+                        <label className="w-full">Title:</label>
+                        <input type="text" name="title" placeholder="Title" className="w-full p-2 rounded bg-gray-700 my-2" />
                     </div>
                     <div>
-                        <label>Content:</label>
-                        <textarea rows="10" cols="30" placeholder="Content" />
-                    </div>
-                    <div>
-                        <input type="submit" value="Send" />
+                        <label className="w-full">Content:</label>
+                        <textarea name="content" rows="10" cols="30" placeholder="Content" className="w-full p-2 rounded bg-gray-700 my-2" />
                     </div>
                 </form>
             </section>
