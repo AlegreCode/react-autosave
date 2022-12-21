691003e3386b5cc9a5073b96aa5c7c0c5a151632
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2022-12-21 18:25:02 -0300

    Añadimos un estado al componente y creamos la función handleChange

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,5 +1,13 @@
-
+import { useState } from "react";
 function Form() {
+    const [form, setForm] = useState({
+        title: '',
+        content: ''
+    });
+
+    const handleChange = (name, value) => {
+        setForm({...form, [name]: value});
+    }
     return (
         <>
             <section className="text-white w-[600px] p-4">
@@ -7,11 +15,15 @@ function Form() {
                 <form>
                     <div>
                         <label className="w-full">Title:</label>
-                        <input type="text" name="title" placeholder="Title" className="w-full p-2 rounded bg-gray-700 my-2" />
+                        <input type="text" name="title" placeholder="Title" className="w-full p-2 rounded bg-gray-700 my-2"
+                        onChange={(e) => handleChange(e.target.name, e.target.value)}
+                        />
                     </div>
                     <div>
                         <label className="w-full">Content:</label>
-                        <textarea name="content" rows="10" cols="30" placeholder="Content" className="w-full p-2 rounded bg-gray-700 my-2" />
+                        <textarea name="content" rows="10" cols="30" placeholder="Content" className="w-full p-2 rounded bg-gray-700 my-2"
+                        onChange={(e) => handleChange(e.target.name, e.target.value)}
+                        />
                     </div>
                 </form>
             </section>
