7be70bcf3f0901cb8a98b23e4a4320fea298299f
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2022-12-23 17:44:04 -0300

    Creamos la barra de estado.

---
+++ src/statusbar/StatusBar.js
@@ -0,0 +1,22 @@
+import UseAnimations from "react-useanimations";
+import loading from "react-useanimations/lib/loading";
+import checkbox from "react-useanimations/lib/checkBox";
+
+function StatusBar(props) {
+    return (
+        <>
+            {props.loading &&
+            <div className="flex justify-center items-center gap-1">
+                <span className="text-green-700">Loading...</span><UseAnimations animation={loading} size={36} strokeColor={"#15803d"} />
+            </div>
+            }
+            {!props.loading &&
+            <div className="flex justify-center items-center gap-1">
+                <span className="text-green-700">Saved</span><UseAnimations reverse={true} animation={checkbox} size={36} strokeColor={"#15803d"} autoplay={true} />
+            </div>
+            }
+        </>
+    )
+}
+
+export default StatusBar;
